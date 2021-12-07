c1306b6618abd8a859869ee7b07d30b0aa3ae2a3
aleks-yustas <0f4c82@gmail.com>
2021-12-07 19:10:13 +0300

    Тест шаблонизатора

--- source/js/data.js
+++ source/js/data.js
@@ -3,5 +3,10 @@ export default {
     { "id": 0, "path": "index.html", "name": "Главная" },
     { "id": 1, "path": "catalog.html", "name": "Каталог продукции" },
     { "id": 2, "path": "form.html", "name": "Подбор программы" }
+  ],
+  "socials": [
+    { "link": "https://vk.com/htmlacademy", "name": "Кэт энерджи во Вконтакте" },
+    { "link": "https://www.instagram.com/htmlacademy/", "name": "Кэт энерджи в Инстаграм" },
+    { "link": "https://www.facebook.com/htmlacademy", "name": "Кэт энерджи в Фейсбуке" }
   ]
 }
