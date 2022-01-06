28d73fe7ac064a69e5eea53147122ac748d1f91f
aleks-yustas <0f4c82@gmail.com>
2022-01-06 20:21:59 +0300

    Адаптивная сетка главной страницы

--- source/templates/data.js
+++ source/templates/data.js
@@ -8,5 +8,14 @@ export default {
     { "id": "vk", "link": "https://vk.com/htmlacademy", "name": "Кэт энерджи во Вконтакте" },
     { "id": "insta", "link": "https://www.instagram.com/htmlacademy/", "name": "Кэт энерджи в Инстаграм" },
     { "id": "fb", "link": "https://www.facebook.com/htmlacademy", "name": "Кэт энерджи в Фейсбуке" }
+  ],
+  "catalogItems": [
+    { "id": 0, "title": "Cat Energy PRO 500 г", "image": "pro-chiken-500", "weight": 500, "tasty": "Курица", "price": 700 },
+    { "id": 1, "title": "Cat Energy PRO 1000 г", "image": "pro-chiken-1000", "weight": 1000, "tasty": "Курица", "price": 1000 },
+    { "id": 2, "title": "Cat Energy PRO 500 г", "image": "pro-fish-500", "weight": 500, "tasty": "Рыба", "price": 700 },
+    { "id": 3, "title": "Cat Energy PRO 1000 г", "image": "pro-fish-1000", "weight": 1000, "tasty": "Рыба", "price": 1000 },
+    { "id": 4, "title": "Cat Energy Slim 500 г", "image": "slim-buckwheat-500", "weight": 500, "tasty": "Гречка", "price": 400 },
+    { "id": 5, "title": "Cat Energy Slim 1000 г", "image": "slim-buckwheat-1000", "weight": 1000, "tasty": "Гречка", "price": 700 },
+    { "id": 6, "title": "Cat Energy Slim 500 г", "image": "slim-rice-500", "weight": 500, "tasty": "Рис", "price": 500 }
   ]
 }
