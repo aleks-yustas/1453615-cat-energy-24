e1bbc38a44acf31deea8442334312fb88bfd14d1
aleks-yustas <0f4c82@gmail.com>
2022-01-20 06:05:32 +0300

    Сетка внутренних страниц

--- source/templates/data.js
+++ source/templates/data.js
@@ -10,12 +10,18 @@ export default {
     { "id": "fb", "link": "https://www.facebook.com/htmlacademy", "name": "Кэт энерджи в Фейсбуке" }
   ],
   "catalogItems": [
-    { "id": 0, "title": "Cat Energy PRO 500 г", "image": "pro-chiken-500", "weight": 500, "tasty": "Курица", "price": 700 },
-    { "id": 1, "title": "Cat Energy PRO 1000 г", "image": "pro-chiken-1000", "weight": 1000, "tasty": "Курица", "price": 1000 },
-    { "id": 2, "title": "Cat Energy PRO 500 г", "image": "pro-fish-500", "weight": 500, "tasty": "Рыба", "price": 700 },
-    { "id": 3, "title": "Cat Energy PRO 1000 г", "image": "pro-fish-1000", "weight": 1000, "tasty": "Рыба", "price": 1000 },
-    { "id": 4, "title": "Cat Energy Slim 500 г", "image": "slim-buckwheat-500", "weight": 500, "tasty": "Гречка", "price": 400 },
-    { "id": 5, "title": "Cat Energy Slim 1000 г", "image": "slim-buckwheat-1000", "weight": 1000, "tasty": "Гречка", "price": 700 },
-    { "id": 6, "title": "Cat Energy Slim 500 г", "image": "slim-rice-500", "weight": 500, "tasty": "Рис", "price": 500 }
+    { "id": 0, "title": "Cat Energy PRO", "image": "pro-chiken-500", "weight": 500, "tasty": "Курица", "price": 700 },
+    { "id": 1, "title": "Cat Energy PRO", "image": "pro-chiken-1000", "weight": 1000, "tasty": "Курица", "price": 1000 },
+    { "id": 2, "title": "Cat Energy PRO", "image": "pro-fish-500", "weight": 500, "tasty": "Рыба", "price": 700 },
+    { "id": 3, "title": "Cat Energy PRO", "image": "pro-fish-1000", "weight": 1000, "tasty": "Рыба", "price": 1000 },
+    { "id": 4, "title": "Cat Energy Slim", "image": "slim-buckwheat-500", "weight": 500, "tasty": "Гречка", "price": 400 },
+    { "id": 5, "title": "Cat Energy Slim", "image": "slim-buckwheat-1000", "weight": 1000, "tasty": "Гречка", "price": 700 },
+    { "id": 6, "title": "Cat Energy Slim", "image": "slim-rice-500", "weight": 500, "tasty": "Рис", "price": 500 }
+  ],
+  "additionalItems": [
+    { "id": 0, "title": "Сахарозаменитель", "packaging": "1 упаковка (100 г)", "price": 200 },
+    { "id": 1, "title": "Питьевая вода", "packaging": "5 литров", "price": 50 },
+    { "id": 2, "title": "Молоко", "packaging": "1 литр", "price": 100 },
+    { "id": 3, "title": "Витамины", "packaging": "1 упаковка (30 г)", "price": 300 }
   ]
 }
