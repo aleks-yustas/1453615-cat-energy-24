3f3f2d4c8412a6f7bdaf88e90882f132ce078049
aleks-yustas <0f4c82@gmail.com>
2022-02-02 05:59:57 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,34 +1,34 @@
 (function() {
   let jsInit = function () {
-    let hiddenElements = document.querySelectorAll('[hidden]');
+    let hiddenElements = document.querySelectorAll("[hidden]");
 
     for (let i = 0; i < hiddenElements.length; i++) {
-      hiddenElements[i].removeAttribute('hidden');
+      hiddenElements[i].removeAttribute("hidden");
     }
   }
 
-  let nav = document.querySelector('.main-nav');
-  let navToggler = nav.querySelector('.main-nav__toggler');
-  let navList = nav.querySelector('.main-nav__list');
+  let nav = document.querySelector(".main-nav");
+  let navToggler = nav.querySelector(".main-nav__toggler");
+  let navList = nav.querySelector(".main-nav__list");
 
   let navInit = function () {
-    nav.classList.remove('main-nav--no-js');
+    nav.classList.remove("main-nav--no-js");
 
-    if (!navToggler.classList.contains('main-nav__toggler--closed')) {
-      navToggler.classList.add('main-nav__toggler--closed');
+    if (!navToggler.classList.contains("main-nav__toggler--closed")) {
+      navToggler.classList.add("main-nav__toggler--closed");
     }
 
-    if (!navList.classList.contains('main-nav__list--closed')) {
-      navList.classList.add('main-nav__list--closed');
+    if (!navList.classList.contains("main-nav__list--closed")) {
+      navList.classList.add("main-nav__list--closed");
     }
   };
 
   let openNav = function () {
-    navToggler.classList.toggle('main-nav__toggler--closed');
-    navList.classList.toggle('main-nav__list--closed');
+    navToggler.classList.toggle("main-nav__toggler--closed");
+    navList.classList.toggle("main-nav__list--closed");
   }
 
   jsInit();
   navInit();
-  nav.addEventListener('click', openNav);
+  nav.addEventListener("click", openNav);
 }());
